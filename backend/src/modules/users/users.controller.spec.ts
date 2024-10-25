6bf7b572e84a03b05b636eb694b6b8ddef4e50e4
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-25 10:13:24 -0500

    Update naming in tests

--- backend/src/modules/users/users.controller.spec.ts
+++ backend/src/modules/users/users.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 
-describe('UserController', () => {
+describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
