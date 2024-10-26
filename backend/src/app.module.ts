10170a0703c5c26cfe1d76db96bc639ff840927f
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-26 18:13:18 -0500

    Securely manage env variables with config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,26 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { FundsModule } from './modules/funds/funds.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { env } from './config/env';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
-
-const connection = env.mongoConnection;
-const username = env.username;
-const password = env.password;
-const host = env.mongoHost;
-const database = env.database;
-
-const mongoConnection = `${connection}://${username}:${password}@${host}`;
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database/database.module';
+import config from './config/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(mongoConnection, { dbName: database }),
+    ConfigModule.forRoot({
+      load: [config],
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     UsersModule,
     FundsModule,
     TransactionsModule,
     NotificationsModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
