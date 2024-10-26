19df0a7e8305758cc672cc4ce8953b21892a5ec4
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-26 14:29:03 -0500

    Remove AWS SES client package

--- backend/src/modules/notifications/notifications.service.ts
+++ backend/src/modules/notifications/notifications.service.ts
@@ -1,49 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 import { env } from '../../config/env';
 import { NotificationMethod, User } from '../users/schemas/user.schema';
 
 @Injectable()
 export class NotificationsService {
-  sesClient: SESClient;
-  constructor() {
-    this.sesClient = new SESClient({
-      region: env.awsRegion,
-      credentials: {
-        accessKeyId: env.awsAccessKeyId, // TODO: Replace with your actual access key
-        secretAccessKey: env.awsSecretAccessKey,
-      },
-    });
-  }
-  createSendEmailCommand(
-    userAddress: string,
-    userName: string,
-    fundName: string,
-  ) {
-    // return new SendEmailCommand({
-    //   Destination: {
-    //     ToAddresses: [userAddress],
-    //   },
-    //   Message: {
-    //     Body: {
-    //       //   Html: {
-    //       //     Charset: 'UTF-8',
-    //       //     Data: 'TEXT_FORMAT_BODY',
-    //       //   },
-    //       Text: {
-    //         Charset: 'UTF-8',
-    //         Data: 'TEXT_FORMAT_BODY',
-    //       },
-    //     },
-    //     Subject: {
-    //       Charset: 'UTF-8',
-    //       Data: 'EMAIL_SUBJECT',
-    //     },
-    //   },
-    //   Source: 'fromAddress@gmail.com',
-    // });
-  }
-
   async sendNotification(user: User, fundName: string) {
     if (user.notificationMethod === NotificationMethod.EMAIL) {
       return await this.sendNoticationbyEmail(user, fundName);
@@ -51,20 +11,7 @@ export class NotificationsService {
     return await this.sendNotificationBySms(user, fundName);
   }
 
-  async sendNoticationbyEmail(user: User, fundName: string) {
-    const sendEmailCommand = this.createSendEmailCommand(
-      user.email,
-      user.firstName,
-      fundName,
-    );
-
-    try {
-      // return await this.sesClient.send(sendEmailCommand);
-    } catch (e) {
-      console.error('Failed to send email.');
-      return e;
-    }
-  }
+  async sendNoticationbyEmail(user: User, fundName: string) {}
 
   async sendNotificationBySms(user: User, fundName: string) {
     if (user.notificationMethod === NotificationMethod.SMS) {
