27fa9dfbbd876c9ab60e46cbe04ef460a1f891a9
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-26 12:25:09 -0500

    Mock services in subscriptions service test

--- backend/src/modules/users/subscriptions/subscriptions.service.spec.ts
+++ backend/src/modules/users/subscriptions/subscriptions.service.spec.ts
@@ -4,22 +4,47 @@ import { Model } from 'mongoose';
 import { getModelToken } from '@nestjs/mongoose';
 import { Subscription } from './schemas/subscription.schema';
 import { subscriptionModelMockProvider } from '../../../../test/mocks/subscriptions.service';
-import { TransactionsModule } from '../../transactions/transactions.module';
-import { FundsModule } from '../../funds/funds.module';
+import { TransactionsService } from '../../transactions/transactions.service';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { FundsService } from '../../funds/funds.service';
+import { userModelMockProvider } from '../../../../test/mocks/users.service';
+import { UsersService } from '../users.service';
 
 describe('SubscriptionsService', () => {
   let service: SubscriptionsService;
   let subscriptionModel: Model<Subscription>;
+  let usersService: DeepMocked<UsersService>;
+  let transactionsService: DeepMocked<TransactionsService>;
+  let fundsService: DeepMocked<FundsService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SubscriptionsService, subscriptionModelMockProvider],
+      providers: [
+        SubscriptionsService,
+        subscriptionModelMockProvider,
+        {
+          provide: UsersService,
+          useValue: createMock<UsersService>(),
+        },
+        {
+          provide: TransactionsService,
+          useValue: createMock<TransactionsService>(),
+        },
+        {
+          provide: FundsService,
+          useValue: createMock<FundsService>(),
+        },
+      ],
     }).compile();
 
     service = module.get<SubscriptionsService>(SubscriptionsService);
     subscriptionModel = module.get<Model<Subscription>>(
       getModelToken(Subscription.name),
     );
+    usersService = module.get<DeepMocked<UsersService>>(UsersService);
+    transactionsService =
+      module.get<DeepMocked<TransactionsService>>(TransactionsService);
+    fundsService = module.get<DeepMocked<FundsService>>(FundsService);
   });
 
   it('should be defined', () => {
