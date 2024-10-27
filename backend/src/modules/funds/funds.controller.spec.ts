73d774b3ee80ace5007272a305b4b83ca2b6f964
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-26 23:52:48 -0500

    Add funds controller tests

--- backend/src/modules/funds/funds.controller.spec.ts
+++ backend/src/modules/funds/funds.controller.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FundsController } from './funds.controller';
 import { FundsService } from './funds.service';
-import { createMock } from '@golevelup/ts-jest';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { mockFund } from '../../../test/mocks/funds.service';
+import { stubMongoId } from '../../../test/mocks/utility.mock';
 
 describe('FundsController', () => {
   let controller: FundsController;
+  let service: DeepMocked<FundsService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,9 +21,32 @@ describe('FundsController', () => {
     }).compile();
 
     controller = module.get<FundsController>(FundsController);
+    service = module.get<DeepMocked<FundsService>>(FundsService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should get funds', async () => {
+    jest.spyOn(service, 'getFunds').mockResolvedValueOnce([mockFund() as any]);
+    const funds = await controller.getFunds();
+    expect(funds).toEqual([mockFund()]);
+  });
+
+  it('should get fund by id', async () => {
+    jest.spyOn(service, 'getFundById').mockResolvedValueOnce(mockFund() as any);
+    const fund = await controller.getFundById(stubMongoId);
+    expect(fund).toEqual(mockFund());
+  });
+
+  it('should create fund', async () => {
+    jest.spyOn(service, 'createFund').mockResolvedValueOnce(mockFund() as any);
+    const fund = await controller.createFund(mockFund());
+    expect(fund).toEqual(mockFund());
   });
 });
