f0b62477322c6f6f74bf9b3d73478516f11b0b2b
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-26 23:55:36 -0500

    Add tests for transactions controller

--- backend/src/modules/transactions/transactions.controller.spec.ts
+++ backend/src/modules/transactions/transactions.controller.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
-import { createMock } from '@golevelup/ts-jest';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { mockTransaction } from '../../../test/mocks/transactions.service';
 
 describe('TransactionsController', () => {
   let controller: TransactionsController;
+  let service: DeepMocked<TransactionsService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,9 +20,23 @@ describe('TransactionsController', () => {
     }).compile();
 
     controller = module.get<TransactionsController>(TransactionsController);
+    service = module.get<DeepMocked<TransactionsService>>(TransactionsService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should get transactions', async () => {
+    const mockedTransaction = mockTransaction(new Date());
+    jest
+      .spyOn(service, 'getTransactions')
+      .mockResolvedValueOnce([mockedTransaction as any]);
+    const transactions = await controller.getTransactions({});
+    expect(transactions).toEqual([mockedTransaction as any]);
   });
 });
