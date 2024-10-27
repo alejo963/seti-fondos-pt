21e06c98f22c5ae6a33ca03888a1399b22366ef5
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-26 23:50:03 -0500

    Add users controller tests

--- backend/src/modules/users/users.controller.spec.ts
+++ backend/src/modules/users/users.controller.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
-import { createMock } from '@golevelup/ts-jest';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { UsersService } from './users.service';
 import { SubscriptionsService } from './subscriptions/subscriptions.service';
+import { mockUser } from '../../../test/mocks/users.service';
+import { mockSubscription } from '../../../test/mocks/subscriptions.service';
+import { stubMongoId } from '../../../test/mocks/utility.mock';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: DeepMocked<UsersService>;
+  let subscriptionService: DeepMocked<SubscriptionsService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,9 +28,64 @@ describe('UsersController', () => {
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<DeepMocked<UsersService>>(UsersService);
+    subscriptionService =
+      module.get<DeepMocked<SubscriptionsService>>(SubscriptionsService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should get user by id', async () => {
+    jest.spyOn(service, 'getUser').mockResolvedValueOnce(mockUser() as any);
+    const user = await controller.getUser(mockUser()._id);
+    expect(user).toEqual(mockUser());
+  });
+
+  it('should create user', async () => {
+    jest.spyOn(service, 'createUser').mockResolvedValueOnce(mockUser() as any);
+    const user = await controller.createUser(mockUser() as any);
+    expect(user).toEqual(mockUser());
+  });
+
+  it('should subscribe user', async () => {
+    jest
+      .spyOn(subscriptionService, 'subscribeToFund')
+      .mockResolvedValueOnce(mockSubscription() as any);
+    const subscription = await controller.subscribeToFund(stubMongoId, {
+      fund: stubMongoId,
+      amount: 200000,
+    });
+    expect(subscription).toEqual(mockSubscription());
+  });
+
+  it('should get user subscriptions', async () => {
+    jest
+      .spyOn(subscriptionService, 'getUserSubscriptions')
+      .mockResolvedValueOnce([mockSubscription() as any]);
+    const subscriptions = await controller.getUserSubscriptions(stubMongoId);
+    expect(subscriptions).toEqual([mockSubscription()]);
+  });
+
+  it('should delete user subscription', async () => {
+    jest
+      .spyOn(subscriptionService, 'unsubscribeFromFund')
+      .mockResolvedValueOnce({
+        acknowledged: true,
+        deletedCount: 1,
+      });
+    const subscription = await controller.unsubscribeFromFund(
+      stubMongoId,
+      stubMongoId,
+    );
+    expect(subscription).toEqual({
+      acknowledged: true,
+      deletedCount: 1,
+    });
   });
 });
