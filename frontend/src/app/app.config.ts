4a69cf5c4c49cb0c74e96a1c04fbfa4fb1bbae0d
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-27 23:20:27 -0500

    Add modal and create subscriptions

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,47 +1,22 @@
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-  importProvidersFrom,
-} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withInMemoryScrolling,
-} from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 // icons
 import { TablerIconsModule } from 'angular-tabler-icons';
 import * as TablerIcons from 'angular-tabler-icons/icons';
 
-// perfect scrollbar
-import { NgScrollbarModule } from 'ngx-scrollbar';
 //Import all material modules
 import { MaterialModule } from './material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled',
-      }),
-      withComponentInputBinding()
-    ),
+    provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
 
-    importProvidersFrom(
-      FormsModule,
-      ReactiveFormsModule,
-      MaterialModule,
-      TablerIconsModule.pick(TablerIcons),
-      NgScrollbarModule
-    ),
+    importProvidersFrom(MaterialModule, TablerIconsModule.pick(TablerIcons)),
   ],
 };
--- frontend/src/app/pages/funds/funds.component.ts
+++ frontend/src/app/pages/funds/funds.component.ts
@@ -6,6 +6,12 @@ import { Fund } from '../../shared/models/fund.model';
 import { CommonModule } from '@angular/common';
 import { SubscriptionsService } from '../../shared/services/subscriptions.service';
 import { SubscribeUserDto } from '../../shared/dtos/subscription.dto';
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-funds',
@@ -14,10 +20,13 @@ import { SubscribeUserDto } from '../../shared/dtos/subscription.dto';
   templateUrl: './funds.component.html',
 })
 export class AppFundsComponent {
+  dialogConfig = new MatDialogConfig();
+  modalDialog: MatDialogRef<ModalComponent, any> | undefined;
   funds = signal<Fund[]>([]);
   constructor(
     private fundsService: FundsService,
-    private subscriptionsService: SubscriptionsService
+    private subscriptionsService: SubscriptionsService,
+    public matDialog: MatDialog
   ) {}
 
   ngOnInit() {
@@ -28,15 +37,31 @@ export class AppFundsComponent {
     });
   }
 
-  subscribeToFund(fundId: string) {
-    const payload: SubscribeUserDto = { amount: 125000, fund: fundId };
-    this.subscriptionsService.subscribeToFund(
-      '671e6f4d59067cabbc071b5d',
-      payload
-    ).subscribe({
-      next: (response) => {
-        
+  openModal(fundId: string) {
+    this.dialogConfig.id = 'modal-component';
+    this.dialogConfig.height = '300px';
+    this.dialogConfig.width = '500px';
+    this.modalDialog = this.matDialog.open(ModalComponent, this.dialogConfig);
+
+    this.modalDialog.afterClosed().subscribe((amount: number) => {
+      if (amount) {
+        this.subscribeToFund(fundId, amount);
       }
     });
+  }
+
+  subscribeToFund(fundId: string, amount: number) {
+    const payload: SubscribeUserDto = { amount, fund: fundId };
+
+    console.log(fundId);
+    this.subscriptionsService
+      .subscribeToFund('671e6f4d59067cabbc071b5d', payload)
+      .subscribe({
+        next: (response) => {},
+      });
+  }
+
+  handleCloseModal(data: any) {
+    console.log(data);
   }
 }
