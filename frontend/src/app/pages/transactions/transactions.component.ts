00b5d8f31f501576213ac977a463517a0d39c104
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-27 19:28:24 -0500

    Add paginator

--- frontend/src/app/pages/transactions/transactions.component.ts
+++ frontend/src/app/pages/transactions/transactions.component.ts
@@ -1,52 +1,15 @@
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { ThemePalette } from '@angular/material/core';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  signal,
-} from '@angular/core';
-import {
-  CdkDrag,
-  CdkDragDrop,
-  CdkDropList,
-  moveItemInArray,
-} from '@angular/cdk/drag-drop';
-import { MatCardModule } from '@angular/material/card';
-import {
-  MatChipEditedEvent,
-  MatChipInputEvent,
-  MatChipsModule,
-} from '@angular/material/chips';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { MatButtonModule } from '@angular/material/button';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { TransactionsService } from '../../shared/services/transactions.service';
 import { Transaction } from '../../shared/models/transaction.model';
-import { TablerIconsModule } from 'angular-tabler-icons';
 import { MaterialModule } from '../../material.module';
-
-export interface ChipColor {
-  name: string;
-  color: ThemePalette;
-}
-
-export interface Fruit {
-  name: string;
-}
-
-export interface Vegetable {
-  name: string;
-}
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
   templateUrl: './transactions.component.html',
-  imports: [MaterialModule, TablerIconsModule, CommonModule],
+  imports: [MaterialModule, CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppTransactionsComponent {
