6052dd64432603ed0423ffc58de5da65290fb0df
Alejandro Velásquez <v.alejo96@gmail.com>
2024-10-28 02:47:28 -0500

    Add test user from aws instance

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'http://18.219.163.59',
-    testUserId: '671e6f4d59067cabbc071b5d'
+    testUserId: '671f41551da5ba5bbbc31b7d'
   };
